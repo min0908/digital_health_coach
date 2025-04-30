70a8ef8f262016b508fc5a67657f7803e607a6a9
min0908 <94737255+min0908@users.noreply.github.com>
2025-04-30 17:39:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ if user_input:
 
             with col1:
                 st.image(
-                    "https://github.com/min0908/digital_health_coach/blob/main/data/digital_health_coach_image.png",
+                    "https://github.com/min0908/digital_health_coach/blob/main/data/digital_health_coach_image.png?raw=true",
                     width=50,
                 )
 
